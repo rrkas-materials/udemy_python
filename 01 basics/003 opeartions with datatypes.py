61a24f3d859100cebe42218ec3456161fc0f4f48
rrkas <rrka97wal@gmail.com>
2020-08-05 21:38:42 +0530

    basic oprations on datatypes

---
+++ 01 basics/003 opeartions with datatypes.py
@@ -0,0 +1,49 @@
+#LISTS
+
+# temps = [1,2,3,4,5]
+# print(temps)                #[1, 2, 3, 4, 5]
+# temps.append(6)
+# print(temps)                #[1, 2, 3, 4, 5, 6]
+# temps.clear()
+# print(temps)                #[]
+# temps= [1,2,3,4,5]
+# print(temps)                #[1, 2, 3, 4, 5]
+# idx = temps.index(2)
+# print(idx)                  #1
+# idx = temps.index(2,1)
+# print(idx)                  #1
+# idx = temps.index(2,1,2)
+# print(idx)                  #1
+# item = temps[1]
+# print(item)                 #2
+# items = temps[1:3]          #upper limit is not included in slice
+# print(items)                #[2, 3]
+# items = temps[1:]           #from idx 1 till last
+# print(items)                #[2, 3, 4, 5]
+# items = temps[:3]           #from idx 0 till (upper-1) ; upper=3,here
+# print(items)                #[1, 2, 3]
+# item = temps[-1]            #negative indexing starts from last, ie: [-len, -len+1, ... , -1]
+# print(item)                 #5
+# items = temps[-2:]
+# print(items)                #[4, 5]
+# items = temps[-5:-2]
+# print(items)                #[1, 2, 3]
+
+
+#----------------------------------------------------------------------------------------------------------
+#STRINGS
+
+# hello = 'Hello'
+# print(hello[1])                 #e
+# print(hello[-1])                #o
+# print(hello[2:4])               #ll
+# list2 = ['Hello', 2, 3]
+# print(list2[0][1])              #e
+
+
+#----------------------------------------------------------------------------------------------------------
+#DICTS
+
+# mydict ={"Marry":9.1, "Sim":8.8, "John":7.5}
+# print(mydict["Marry"])          #9.1
+
