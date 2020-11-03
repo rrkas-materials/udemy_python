28a56d553066144d573577bbe87cf317a788b90d
rrkas <rrka97wal@gmail.com>
2020-11-04 00:06:55 +0530

    app5 started - GUI

---
+++ 06 app5 - desktop db app/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()  # start of gui part
+
+b1 = Button(window, text="Execute")
+b1.grid(row=0, column=0, rowspan=2)
+
+e1 = Entry(window)
+e1.grid(row=0, column=1)
+
+t1 = Text(window, height=1, width=20)
+t1.grid(row=0, column=2)
+window.mainloop()  # ends of gui part
